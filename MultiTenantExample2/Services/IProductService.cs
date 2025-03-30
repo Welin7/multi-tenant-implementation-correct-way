55e3f421b91eee52c12b300608b444149a4e8abe
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-03-30 19:23:28 -0300

    Created class of service of product.
    Created class and interface to service of product.

---
+++ MultiTenantExample2/Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiTenantExample2.Services
+{
+    public interface IProductService
+    {
+        Task<Product>CreateAsync(Product product);
+        Task<Product?> GetByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetAllAsync();
+    }
+}
