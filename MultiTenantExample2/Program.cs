2667739cd3245361115b69c0c0e6781c27f70625
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-03-30 11:21:44 -0300

    Register of interfaces in Program.cs.
    Register of interfaces of tenant and IHttpContextAccessor. Configuration the file appsettings.json with the tenants,

--- MultiTenantExample2/Program.cs
+++ MultiTenantExample2/Program.cs
@@ -1,6 +1,13 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped<ITenantService, TenantService>();
+builder.Services.Configure<TenantSettings>(builder.Configuration.GetSection(nameof(TenantSettings)));
+
+TenantSettings options = new();
+builder.Configuration.GetSection(nameof(TenantSettings)).Bind(options);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
