1df7bfc8df1674c72988b7f7aea572da462ec687
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-03-30 21:25:29 -0300

    Add swagger on the project.
    Add configuration of swagger on the project.

--- MultiTenantExample2/Program.cs
+++ MultiTenantExample2/Program.cs
@@ -43,6 +43,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+        options.SwaggerEndpoint("/openapi/v1.json", "MultiTenant For Multiple Databases(Scheme)"));
 }
 
 app.UseHttpsRedirection();
